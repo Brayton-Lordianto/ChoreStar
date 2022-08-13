20018f7d0ce748e1a37c081ed2698bd61854707f
Brayton-Lordianto <braylord30@gmail.com>
2022-08-13 19:11:49 +0800

    Added Location Manager Files. References from online.

---
+++ ChoreStar/TestViews/LocationView.swift
@@ -0,0 +1,38 @@
+//
+//  LocationView.swift
+//  ChoreStar
+//
+//  Created by Brayton Lordianto on 8/13/22.
+//
+
+import SwiftUI
+
+// attempt to get the location of the user
+
+struct LocationView: View {
+    @StateObject var locationManager = LocationManager()
+    
+    var userLatitude: String {
+        return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
+    }
+    
+    var userLongitude: String {
+        return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
+    }
+    
+    var body: some View {
+        VStack {
+            Text("location status: \(locationManager.statusString)")
+            HStack {
+                Text("latitude: \(userLatitude)")
+                Text("longitude: \(userLongitude)")
+            }
+        }
+    }
+}
+
+struct LocationView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationView()
+    }
+}
