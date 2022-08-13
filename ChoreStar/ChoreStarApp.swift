3fe11e25a1bdf8b09ff1a1b28e2666c71c07c714
Brayton-Lordianto <braylord30@gmail.com>
2022-08-13 14:52:16 +0800

    Initial Commit

---
+++ ChoreStar/ChoreStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoreStarApp.swift
+//  ChoreStar
+//
+//  Created by Brayton Lordianto on 8/13/22.
+//
+
+import SwiftUI
+
+@main
+struct ChoreStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChoreStar/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ChoreStar
+//
+//  Created by Brayton Lordianto on 8/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
